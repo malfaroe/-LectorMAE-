030fd28606d3f4737cc6e5d2e0326b4c113b4e26
malfaroe <mauricio.alfaro.espinoza@gmail.com>
2026-05-20 11:02:52 -0400

    Add Gradle wrapper + fix CI build

---
+++ gradlew.bat
@@ -0,0 +1,19 @@
+@rem Gradle wrapper script for Windows
+@if "%DEBUG%" == "" @echo off
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" setlocal
+
+set APP_HOME=%~dp0
+set CLASSPATH=%APP_HOME%gradle\wrapper\gradle-wrapper.jar
+
+set JAVA_EXE=java.exe
+if defined JAVA_HOME set JAVA_EXE=%JAVA_HOME%\bin\java.exe
+
+"%JAVA_EXE%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+
+:end
+if "%ERRORLEVEL%"=="0" goto mainEnd
+:fail
+exit /b 1
+:mainEnd
+if "%OS%"=="Windows_NT" endlocal
